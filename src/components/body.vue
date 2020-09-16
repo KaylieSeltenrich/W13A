315efbcdda875594741eb1bc57b61dfb4e5f8d8f
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-09-15 21:03:37 -0600

    W13A Vue Project

---
+++ src/components/body.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div
+    class="page-body">
+   <side-right />
+    </div>
+  
+</template>
+
+
+<script>
+import SideRight from "./sideright.vue"
+
+export default {
+  name: "page-body",
+  components: {
+  SideRight
+  }
+};
+</script>
+
+<style scoped>
+.page-body {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  grid-template-columns: 1fr;
+  margin: 10%;
+}
+</style>
---
+++ src/components/sideright.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div class="side-right">
+    I am on the side of the page on the right
+ </div>
+ 
+</template>
+
+
+<script>
+
+export default {
+  name: "side-right",
+};
+
+</script>
+
+<style scoped>
+
+</style>
